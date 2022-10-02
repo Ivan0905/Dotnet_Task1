740b0c811864063b21e67dab2744beba33909b38
ivan <ia.farafonov@yandex.ru>
2022-10-02 12:57:42 +0300

    Выполнено задание №8

--- Program.cs
+++ Program.cs
@@ -37,3 +37,14 @@ if (N1 % 2 == 0) { Console.WriteLine($"Число {N1} является четн
 else { Console.WriteLine($"Число {N1} является нечетным"); }
 */
 
+// Задание №8 Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число ");
+int N = int.Parse(Console.ReadLine()!);
+int i = 1;
+
+while (i<=N){
+if (i%2==0) {
+    Console.WriteLine(i);
+} 
+i++;
+}
