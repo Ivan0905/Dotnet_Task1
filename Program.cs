7dda3efc67bbefe62b586406fa2bd6ca7012b7f3
ivan <ia.farafonov@yandex.ru>
2022-10-02 12:36:52 +0300

    Сделана задача №4

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿//Задача №2 Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
 /*Console.WriteLine("Введите первое число ");
 int N1 = int.Parse(Console.ReadLine()!);
@@ -9,3 +9,18 @@ if (N1 > N2) { Console.WriteLine($"Max={N1}, Min={N2}"); }
 if (N2 > N1) { Console.WriteLine($"Max={N2}, Min={N1}"); }
 if (N1 == N2) { Console.WriteLine("Введены одинаковые числа"); }
 */
+
+//Задача №4 Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите первое число ");
+int N1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число ");
+int N2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число ");
+int N3 = int.Parse(Console.ReadLine()!);
+int Max = N1;
+
+if (N2 >= Max) { Max=N2; }
+if (N3 >= Max) { Max=N3; }
+
+Console.WriteLine($"Максимальное число = {Max}");   
